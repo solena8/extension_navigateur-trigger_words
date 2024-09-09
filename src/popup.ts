bb5292335984bf5c5a53402ae9246c818b56c7df
Tommyisready <perrot.thomas@live.fr>
2024-09-09 10:18:42 +0200

    html + page option

--- src/popup.ts
+++ src/popup.ts
@@ -25,4 +25,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
             });
         });
     }
+
+    // Event listener for the "Open Options" button
+    const openOptionsButton = document.getElementById(
+        "openOptions"
+    ) as HTMLButtonElement;
+    if (openOptionsButton) {
+        openOptionsButton.addEventListener("click", () => {
+            chrome.tabs.create({ url: "public/options.html" });
+        });
+    }
 });
