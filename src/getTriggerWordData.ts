b5b75132960a78a59e9c8dad4d0b8ad820dcb572
unknown <olivia.bergaglia@gmail.com>
2024-09-09 16:21:09 +0200

    Fetch JSON

--- src/getTriggerWordData.ts
+++ src/getTriggerWordData.ts
@@ -1,11 +1,10 @@
-let jsonData: object | null = null;
-
 export function fetchJSONData(): Promise<any> {
     return fetch(chrome.runtime.getURL('../assets/trigger_db.json'))
         .then(response => response.json())
         .then(data => {
-            jsonData = data;
-            return jsonData;
+            console.log(Object.keys(data.categories[0]));
+            
+            return data;
         })
         .catch(error => {
             console.error('Erreur lors du chargement du JSON:', error);
@@ -13,8 +12,24 @@ export function fetchJSONData(): Promise<any> {
         });
 }
 
+export function afficherAvis(data: any) {
+    const categorie = document.createElement("p");
+    
+    for (let i = 0; i < Object.keys(data.categories).length; i++) {
+        categorie.innerHTML += `<b>${data.categories[i]}:</b><br>`;
+    }
+    document.getElementById("trigger_list")!.appendChild(categorie);
+}
+
 fetchJSONData().then(data => {
-    console.log('Données JSON chargées:', data);
+    window.addEventListener("DOMContentLoaded", (event) => {
+        
+        const header = document.getElementById('trigger_list');
+        if (header) {
+            afficherAvis(data.categories);
+        }
+    });
+
 }).catch(error => {
     console.error('Erreur lors du chargement du JSON:', error);
 });
--- src/popup.ts
+++ src/popup.ts
@@ -2,21 +2,24 @@ import { blockWords } from "./content";
 import { fetchJSONData } from "./getTriggerWordData"; 
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    fetchJSONData();    
+    fetchJSONData();
     
     // Event listener for the "Save Blocked Words" button
     const saveBlockedWordsButton = document.getElementById(
         "saveBlockedWords"
     ) as HTMLButtonElement;
+
     if (saveBlockedWordsButton) {
         saveBlockedWordsButton.addEventListener("click", () => {
             const blockedWordsInput = document.getElementById(
                 "blockedWords"
             ) as HTMLTextAreaElement;
+
             const blockedWords = blockedWordsInput.value
                 .split(",")
                 .map((word) => word.trim())
                 .filter((word) => word.length > 0);
+                
             chrome.storage.sync.set({ blockedWords: blockedWords });
             chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
                 if (tabs[0].id !== undefined) {
@@ -24,7 +27,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
                         target: { tabId: tabs[0].id },
                         func: blockWords, // Inject this function
                     });
-                    // fetchJSONData(); // Inject this function
                     
                 }
             });
